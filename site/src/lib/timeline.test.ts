129145da9f4f97b977441ec234cdf4dc5dcbd510
Wes Johnson <wes@swj.io>
2024-01-28 16:31:40 -0500

    switch primary timeline logic method

--- site/src/lib/timeline.test.ts
+++ site/src/lib/timeline.test.ts
@@ -1,31 +1,13 @@
 import { expect, test } from "vitest";
-import { getTimelineConfig, labelUpdateInterval } from "./timeline";
+import { getTimeline } from "./timeline";
+import { getSvgDomain } from "./server/svg";
+import { getEnhancedPosts } from "./server/posts";
 
-test("getTimelineConfig", () => {
-  const durationSeconds = 120;
-  const killedPerDay = [1000, 2000, 3000, 4000, 5000];
-  const { dayDuration, postDelays, killedLabels } = getTimelineConfig({
-    durationSeconds,
-    posts: [
-      { date: "2024-01-02" },
-      { date: "2024-01-02T03:00:00.000Z" },
-      { date: "2024-01-02T04:00:00.000Z" },
-      { date: "2024-01-05T23:59:00.000Z" }, // end of Jan 5th in EST
-      { date: "2024-01-06T04:59:00.000Z" }, // end of Jan 5th in UTC
-    ],
-    dates: [
-      "2024-01-01",
-      "2024-01-02",
-      "2024-01-03",
-      "2024-01-04",
-      "2024-01-05",
-    ],
-    killedPerDay,
-    wcKilledPerDay: killedPerDay,
-  });
-  expect(dayDuration).toBe(durationSeconds / 5);
-  expect(postDelays).toEqual([24, 24, 24, 115, 120]);
-  expect(killedLabels).toHaveLength(durationSeconds / labelUpdateInterval);
-  expect(killedLabels[0]).toEqual(1000);
-  expect(killedLabels[killedLabels.length - 1]).toEqual(5000);
+test("timeline datamodel integration", () => {
+  const posts = getEnhancedPosts(
+    require("../content/enabler/talbroda.json").posts
+  );
+  const svgDomain = getSvgDomain();
+  const timeline = getTimeline({ svgDomain, posts });
+  expect(timeline).toMatchSnapshot();
 });
