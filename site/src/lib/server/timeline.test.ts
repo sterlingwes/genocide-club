74e8185ec864d79decb2ad5dd686a949426dbf58
Wes Johnson <wes@swj.io>
2024-01-29 16:01:02 -0500

    mock date for spec
    in the time calc there may be subtle drift if our date-fns parsing "copies" certain elements from the runtime clock when they're not defined in our date strings

--- site/src/lib/server/timeline.test.ts
+++ site/src/lib/server/timeline.test.ts
@@ -1,10 +1,17 @@
-import { expect, test } from "vitest";
+import MockDate from "mockdate";
+import { afterAll, beforeAll, expect, test } from "vitest";
 import { getTimeline } from "./timeline";
 import { getSvgDomain } from "./svg";
 import { getEnhancedPosts } from "./posts";
 import dataFixture from "./__fixtures__/data.json";
 
 test("timeline datamodel integration", () => {
+  beforeAll(() => {
+    MockDate.set(new Date(2023, 12, 31, 23, 0, 0, 0));
+  });
+
+  afterAll(() => MockDate.reset());
+
   const posts = getEnhancedPosts(
     require("../../content/enabler/talbroda.json").posts
   );
