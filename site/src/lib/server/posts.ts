558616767f7db68db43c9f67fcf26911f0b516cb
Wes Johnson <wes@swj.io>
2024-01-30 16:39:39 -0500

    better post formatting, handle quotes (#9)
    * extract single post into component
    
    * improve post formatting, handle quotes
    
    * fix workflow event type
    
    * fix incorrect quote
    
    * tweak talbroda json & snapshot timings
    
    * fix local image paths

--- site/src/lib/server/posts.ts
+++ site/src/lib/server/posts.ts
@@ -8,11 +8,76 @@ const orderEarliestToLatest = (posts: EnablerPost[]) => {
   });
 };
 
+type StructuredText =
+  | { type: "text"; text: string }
+  | { type: "url"; text: string; href: string }
+  | { type: "space" }
+  | { type: "break" };
+
+const naiveSingleUrlMatcher = /\s+(http[^\s]+)\s+/;
+
+const formatDisplayUrl = (url: string) => {
+  const [hostPart, ...rest] = url.replace(/https?:\/\//, "").split("/");
+  return `${hostPart}${rest.length ? `/...${rest[rest.length - 1]}` : ""}`;
+};
+
+const splitLinebreaks = (part: StructuredText) => {
+  if (part.type !== "text") {
+    return part;
+  }
+
+  const parts = part.text.split(/\n/);
+  if (parts.length === 1) {
+    return part;
+  }
+
+  return parts.reduce(
+    (acc, part, i) =>
+      parts.length - 1 !== i
+        ? acc.concat({ type: "text", text: part }).concat({ type: "break" })
+        : acc.concat({ type: "text", text: part }),
+    [] as StructuredText[]
+  );
+};
+
+const parsePostText = (text: string): StructuredText[] => {
+  return text
+    .split(naiveSingleUrlMatcher)
+    .map(
+      (part): StructuredText =>
+        typeof part === "object"
+          ? part
+          : part.startsWith("http")
+            ? { type: "url", text: formatDisplayUrl(part), href: part }
+            : { type: "text", text: part }
+    )
+    .reduce(
+      (acc, part, i, allValues) =>
+        allValues.length - 1 !== i
+          ? acc.concat(splitLinebreaks(part)).concat({ type: "space" })
+          : acc.concat(splitLinebreaks(part)),
+      [] as StructuredText[]
+    );
+};
+
 export const getEnhancedPosts = (posts: EnablerPost[]) => {
   return orderEarliestToLatest(posts)
     .filter((post) => !post.skip)
     .map((post) => ({
       ...post,
+      structuredText: parsePostText(post.text),
+      structuredQuoteText: post.quote?.text
+        ? parsePostText(post.quote.text)
+        : [],
+      // prefix locally-hosted images with correct path if not absolute
+      ...(post.image && !post.image.startsWith("http")
+        ? { image: `/post_assets/${post.image}` }
+        : {}),
+      ...(post.quote?.image && !post.quote.image.startsWith("http")
+        ? {
+            quote: { ...post.quote, image: `/post_assets/${post.quote.image}` },
+          }
+        : {}),
       readTime: readingTime(post.text + (post.quote ? post.quote.text : ""), {
         wordsPerMinute: 300,
       }),
