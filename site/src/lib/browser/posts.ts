e615988250d55cea92b489504e4467db0f2b98dc
Wes Johnson <wes@swj.io>
2024-01-30 17:00:31 -0500

    fix post scrolling logic
    corrects issues across viewports but doesn't address updating offsets on resize
    
    closes #6

--- site/src/lib/browser/posts.ts
+++ site/src/lib/browser/posts.ts
@@ -1,30 +1,35 @@
+const postOffsets: Record<number, number> = {};
+const topHeaderAvoidanceShort = 160;
+const topHeaderAvoidanceTall = 100;
+
 export const handlePostEvents = () => {
   const mainElement = document.querySelector("main");
   if (!mainElement) {
     throw new Error("Need main element height for scroll logic");
   }
-  const postTopOffset = mainElement.getBoundingClientRect().height * 0.333;
+  const viewportHeight = mainElement.getBoundingClientRect().height;
   const posts: NodeListOf<HTMLElement> = document.querySelectorAll(".post");
   const postContainer = document.querySelector(".posts");
   if (postContainer) {
-    posts.forEach((post) => {
+    posts.forEach((post, index) => {
+      const postDiv = post as HTMLDivElement | null;
+      if (!postDiv) {
+        console.warn("no post div");
+        return;
+      }
+      const postBoundingRect = postDiv.getBoundingClientRect();
+      postOffsets[index] =
+        postBoundingRect.top -
+        (postBoundingRect.height / viewportHeight > 0.75
+          ? topHeaderAvoidanceTall
+          : topHeaderAvoidanceShort);
       post.addEventListener("animationstart", (event) => {
-        const postDiv = event.target as HTMLDivElement | null;
-        if (!postDiv) {
-          console.warn("no post div");
-          return;
-        }
-
         if (postDiv.classList.contains("post") === false) {
           // child events may bubble up
           return;
         }
 
-        const top =
-          postDiv.getBoundingClientRect().top -
-          postTopOffset +
-          postContainer.scrollTop -
-          100;
+        const top = postOffsets[index];
         postContainer.scrollTo({
           behavior: "smooth",
           top,
